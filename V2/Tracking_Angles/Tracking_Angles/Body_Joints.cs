fae923e689c24972fe7c65a7e9c6684088578e00
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-01 22:19:49 +0900

    Generating new body tracking

---
+++ V2/Tracking_Angles/Tracking_Angles/Body_Joints.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Kinect;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Tracking_Angles
+{
+    public static class Body_Joints
+    {
+    }
+}
---
+++ V2/Tracking_Angles/Tracking_Angles/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Collections.Generic;
+// Kinect dll
+using Microsoft.Kinect;
+
+namespace Tracking_Angles
+{
+    public enum Mode
+    {
+        Color,
+        Depth,
+        Infrared,
+        Body_Joints
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Members 
+
+        Mode _mode = Mode.Color;
+
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+        
+        #endregion
+
+        #region Constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Event handlers
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+                // Read the streams
+                _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color |
+                                                             FrameSourceTypes.Depth |
+                                                             FrameSourceTypes.Infrared);
+                _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+            }
+
+        }
+
+        void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var reference = e.FrameReference.AcquireFrame();
+
+            // Color
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Color)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Depth
+            using (var frame = reference.DepthFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Depth)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Infrared
+            using (var frame = reference.InfraredFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Infrared)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Color;
+        }
+
+        private void Depth_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Depth;
+        }
+
+        private void Infrared_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Infrared;
+        }
+
+        private void Tracking_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Body_Joints;
+        }
+
+        #endregion
+    }
+}
