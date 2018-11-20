206e038193b4662fecfab21964d145024ba42236
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-20 21:49:51 +0900

    point cloud wall

---
+++ V2/Wall/Assets/Scripts/ImageViewer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageViewer : MonoBehaviour {
+
+    // Reference for the texture
+    public MeasureDepth mMeasureDepth;
+
+    // Provide the color and depth data from Kinect
+    public MultiSourceManager mMultiSource;
+
+    // To put the output of the kinect
+    public RawImage mRawImage;
+    public RawImage mRawDepth;
+
+    void Update ()
+    {
+        // Get the color data and display it in the canvas
+        mRawImage.texture = mMultiSource.GetColorTexture();
+        // Here we link the texture with the Viewer
+        mRawDepth.texture = mMeasureDepth.mDepthTexture;
+        	
+	}
+}
---
+++ V2/Wall/Assets/Scripts/MeasureDepth.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class MeasureDepth : MonoBehaviour
+{
+
+    public MultiSourceManager mMultiSource;
+    public Texture2D mDepthTexture;
+
+    private ushort[] mDepthData = null;
+    // Save the 3D points that the camera is reading
+    private CameraSpacePoint[] mCameraSpacePoints = null;
+    // Transform the 3D point to 2D as pixels
+    private ColorSpacePoint[] mColorSpacePoints = null;
+
+    private KinectSensor mSensor = null;
+    private CoordinateMapper mMapper = null;
+
+    private readonly Vector2Int mDepthResolution = new Vector2Int(512, 424);
+
+    private void Awake()
+    {
+        mSensor = KinectSensor.GetDefault();
+        mMapper = mSensor.CoordinateMapper;
+
+        int arraySize = mDepthResolution.x * mDepthResolution.y;
+
+        mCameraSpacePoints = new CameraSpacePoint[arraySize];
+        mColorSpacePoints = new ColorSpacePoint[arraySize];
+    }
+
+    private void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.Space))
+        {
+            DepthToColor();
+
+            mDepthTexture = CreateTexture();
+        }
+    }
+
+    private void DepthToColor()
+    {
+        // Take the depth data
+        mDepthData = mMultiSource.GetDepthData();
+
+        // Map
+        mMapper.MapDepthFrameToCameraSpace(mDepthData, mCameraSpacePoints);
+        mMapper.MapDepthFrameToColorSpace(mDepthData, mColorSpacePoints);
+
+        // Filter
+    }
+
+    private Texture2D CreateTexture()
+    {
+        Texture2D newTexture = new Texture2D(1920, 1080, TextureFormat.Alpha8, false);
+
+        for(int x = 0; x < 1980; x++)
+        {
+            for (int y = 0; y < 1080; y++)
+            {
+                // All the pixels in the texture will clear (transparent)
+                newTexture.SetPixel(x, y, Color.clear);
+            }
+        }
+
+        foreach(ColorSpacePoint point in mColorSpacePoints)
+        {
+            newTexture.SetPixel((int)point.X, (int)point.Y, Color.black);
+        }
+
+        newTexture.Apply();
+
+        return newTexture;
+    }
+
+}
